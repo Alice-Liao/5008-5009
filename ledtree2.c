0d8877fae0b4279d39b21bdf5ce3dd6085e58676
Alice-Liao <111921900+Alice-Liao@users.noreply.github.com>
2023-07-19 10:35:14 -0700

    Update ledtree2.c

--- ledtree2.c
+++ ledtree2.c
@@ -45,70 +45,15 @@ TreeNode* createNode(int led_index) {
     return newNode;
 }
 
-void hsv_to_rgb(float h, float s, float v, uint8_t* r, uint8_t* g, uint8_t* b);  // Function prototype
-
 void traverseList(TreeNode* root) {
     TreeNode* current = root;
 
     int num_leds = ledstring.channel[0].count;
 
-    // Set a different color for each LED
+    // Set the LED colors based on their index
     for (int i = 0; i < num_leds; i++) {
-        float hue = (float)i / num_leds;  // Calculate hue value (0.0 to 1.0)
-
-        // Convert hue to RGB color
-        uint8_t r, g, b;
-        hsv_to_rgb(hue, 1.0, 1.0, &r, &g, &b);
-
-        // Set LED color
-        ledstring.channel[0].leds[i] = (r << 16) | (g << 8) | b;
-    }
-}
-
-void hsv_to_rgb(float h, float s, float v, uint8_t* r, uint8_t* g, uint8_t* b) {
-    if (s == 0.0f) {
-        *r = *g = *b = (uint8_t)(v * 255);
-        return;
-    }
-
-    h *= 6.0;
-    int i = (int)floor(h);
-    float f = h - i;
-    float p = v * (1.0 - s);
-    float q = v * (1.0 - s * f);
-    float t = v * (1.0 - s * (1.0 - f));
-
-    switch (i) {
-        case 0:
-            *r = (uint8_t)(v * 255);
-            *g = (uint8_t)(t * 255);
-            *b = (uint8_t)(p * 255);
-            break;
-        case 1:
-            *r = (uint8_t)(q * 255);
-            *g = (uint8_t)(v * 255);
-            *b = (uint8_t)(p * 255);
-            break;
-        case 2:
-            *r = (uint8_t)(p * 255);
-            *g = (uint8_t)(v * 255);
-            *b = (uint8_t)(t * 255);
-            break;
-        case 3:
-            *r = (uint8_t)(p * 255);
-            *g = (uint8_t)(q * 255);
-            *b = (uint8_t)(v * 255);
-            break;
-        case 4:
-            *r = (uint8_t)(t * 255);
-            *g = (uint8_t)(p * 255);
-            *b = (uint8_t)(v * 255);
-            break;
-        default:
-            *r = (uint8_t)(v * 255);
-            *g = (uint8_t)(p * 255);
-            *b = (uint8_t)(q * 255);
-            break;
+        uint32_t color = i % 2 == 0 ? 0x00FF00 : 0xFF0000;
+        ledstring.channel[0].leds[i] = color;
     }
 }
 
@@ -145,7 +90,7 @@ int main()
     int seconds_elapsed = 0;
 
     while (1) {
-        if (seconds_elapsed < 16) {
+        if (seconds_elapsed < 1) {
             traverseList(root);
         } else {
             // Turn off all LEDs
