5630cb6feb4e233541fc2798b073fba8beabe6f7
Alice-Liao <111921900+Alice-Liao@users.noreply.github.com>
2023-07-19 10:29:24 -0700

    Update ledtree2.c

--- ledtree2.c
+++ ledtree2.c
@@ -45,6 +45,8 @@ TreeNode* createNode(int led_index) {
     return newNode;
 }
 
+void hsv_to_rgb(float h, float s, float v, uint8_t* r, uint8_t* g, uint8_t* b);  // Function prototype
+
 void traverseList(TreeNode* root) {
     TreeNode* current = root;
 
